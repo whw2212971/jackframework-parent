29d6d7c10f320c82512a1bfc4a0a84bda773a1b3
Jack <wotherenty>
2017-11-06 21:47:21 +0800

    Temporary save.

---
+++ jackframework-service/src/main/java/org/jackframework/service/component/ServiceExceptionWrapInterceptor.java
@@ -0,0 +1,31 @@
+package org.jackframework.service.component;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServiceExceptionWrapInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        if (ex instanceof ServiceException) {
+            throw new ServiceServletException(ex);
+        }
+    }
+
+}
