f9580b3e7cd4d7c6b953759638978aaa03a34236
Jack <wotherenty>
2017-11-27 20:10:14 +0800

    Temporary save.

--- jackframework-service/src/main/java/org/jackframework/service/component/ServiceAnnotationHandlerMapping.java
+++ jackframework-service/src/main/java/org/jackframework/service/component/ServiceAnnotationHandlerMapping.java
@@ -9,6 +9,8 @@ import org.springframework.util.ReflectionUtils;
 import org.springframework.web.servlet.handler.AbstractUrlHandlerMapping;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ServiceAnnotationHandlerMapping extends AbstractUrlHandlerMapping implements InitializingBean {
 
@@ -28,20 +30,43 @@ public class ServiceAnnotationHandlerMapping extends AbstractUrlHandlerMapping i
 
     protected void registerHandlers(
             final ApplicationContext context, final String beanName, Class<?> handlerType, final String typeUrlPath) {
+        final Map<String, Method> existsMethods = new HashMap<String, Method>();
         ReflectionUtils.doWithMethods(handlerType, new ReflectionUtils.MethodCallback() {
             @Override
             public void doWith(Method method) {
                 Publish methodPublishApi = AnnotationUtils.findAnnotation(method, Publish.class);
                 if (methodPublishApi != null) {
+                    String methodName   = method.getName();
+                    Method existsMethod = existsMethods.get(methodName);
+                    if (existsMethod != null &&
+                            parameterTypeEquals(existsMethod.getParameterTypes(), method.getParameterTypes())) {
+                        return;
+                    }
                     // typeUrlPath + methodUrlPath
                     String handlerPath = getPathMatcher().combine(typeUrlPath, getUrlPath(methodPublishApi));
                     // create handler
                     ServiceMethodHandler handler = new ServiceMethodHandler(context, beanName, method, handlerPath);
                     // register handler
                     registerHandler(handlerPath, handler);
+
+                    existsMethods.put(method.getName(), method);
                 }
             }
         }, ReflectionUtils.USER_DECLARED_METHODS);
+    }
+
+    protected static boolean parameterTypeEquals(Class<?>[] params1, Class<?>[] params2) {
+        int length = params1.length;
+        if (length != params2.length) {
+            return false;
+        }
+        for (int i = 0; i < length; i++) {
+            if (params1[i].equals(params2[i])) {
+                continue;
+            }
+            return false;
+        }
+        return true;
     }
 
     protected static String getUrlPath(Publish typePublishApi) {
